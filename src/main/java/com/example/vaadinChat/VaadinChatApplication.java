5bb24b58817b8dbefdb38aae4982bd097b883f51
stepaandr013 <stepa.andr@gmail.com>
2022-02-20 00:24:35 +0200

    Initial commit

---
+++ src/main/java/com/example/vaadinChat/VaadinChatApplication.java
@@ -0,0 +1,13 @@
+package com.example.vaadinChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaadinChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaadinChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vaadinChat/VaadinChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vaadinChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaadinChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
